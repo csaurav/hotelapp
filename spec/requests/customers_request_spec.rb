0b3cd96c41c7e7aa23963120c9f39ad4e10c22cc
Saurav C <saurav@>
2020-08-08 18:04:15 +0530

    few more specs of type requests

--- spec/requests/customers_request_spec.rb
+++ spec/requests/customers_request_spec.rb
@@ -1,5 +1,31 @@
-# require 'rails_helper'
+require 'rails_helper'
 
-# RSpec.describe "Customers", type: :request do
+RSpec.describe "Customer", type: :request do
+    let(:room) {
+        Room.create(room_no: "111",
+                    description: "Lorem ipsum",
+                    price: 1000,
+                    room_type: 1
+        )
+    }
+    let(:reservation) {
+        Reservation.create(start_date: '22/01/2020', end_date: '24/01/2020', room_id: room.id)
+    }
+    describe "customer specs" do 
+        it "get list of all customers" do
+            customer = Customer.create(
+                                        first_name: "Abc",
+                                        last_name: "last name",
+                                        address: 'address',
+                                        phone_number: 100000,                        
+                                        reservation_id: reservation.id
+                                    )
+            get customers_path
+            expect(response).to be_successful            
+        end
+        
+       
+    end
+end
 
-# end
+
--- spec/requests/rooms_request_spec.rb
+++ spec/requests/rooms_request_spec.rb
@@ -1,5 +1,20 @@
-# require 'rails_helper'
+require 'rails_helper'
 
-# RSpec.describe "Rooms", type: :request do
+RSpec.describe "Rooms", type: :request do
+    describe "room specs" do 
+        it "get list of all rooms" do
+                room = Room.new(
+                    room_no: "111",
+                    description: "Lorem ipsum",
+                    price: 1000,
+                    room_type: 1
+                )   
+            get "/rooms"
+            expect(response).to be_successful            
+        end
+        
+       
+    end
+end
 
-# end
+
